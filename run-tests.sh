f7b2ee2fcb1f2eb5e57ee4ba82d178ef31c8a8f6
Tibor Šimko <tibor.simko@cern.ch>
2024-01-11 11:45:34 +0100

    ci(commitlint): addition of commit message linter (#192)
    Adds commitlint to check the commit message style against agreed
    conventional commits configuration.
    
    Changes script argument values to always use linter names (e.g.
    shellcheck).
    
    Changes argument handling to allow only one checking action that can now
    accept further optional arguments.

--- run-tests.sh
+++ run-tests.sh
@@ -1,19 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This file is part of REANA.
-# Copyright (C) 2020 CERN.
+# Copyright (C) 2020, 2024 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-# Quit on errors
 set -o errexit
-
-# Quit on unbound symbols
 set -o nounset
 
-check_script () {
-    shellcheck run-tests.sh
+check_commitlint () {
+    from=${2:-master}
+    to=${3:-HEAD}
+    npx commitlint --from="$from" --to="$to"
+    found=0
+    while IFS= read -r line; do
+        if echo "$line" | grep -qP "\(\#[0-9]+\)$"; then
+            true
+        else
+            echo "✖   PR number missing in $line"
+            found=1
+        fi
+    done < <(git log "$from..$to" --format="%s")
+    if [ $found -gt 0 ]; then
+        exit 1
+    fi
+}
+
+check_shellcheck () {
+    find . -name "*.sh" -exec shellcheck {} \;
 }
 
 check_docstyle () {
@@ -27,17 +42,17 @@ build_docs () {
 }
 
 if [ $# -eq 0 ]; then
-    check_script
+    check_commitlint
+    check_shellcheck
     check_docstyle
     build_docs
 fi
 
-for arg in "$@"
-do
-    case $arg in
-        --check-shellscript) check_script;;
-        --check-docstyle) check_docstyle;;
-        --build-docs) build_docs;;
-        *)
-    esac
-done
+arg="$1"
+case $arg in
+    --check-commitlint) check_commitlint "$@";;
+    --check-shellcheck) check_shellcheck;;
+    --check-docstyle) check_docstyle;;
+    --build-docs) build_docs;;
+    *) echo "[ERROR] Invalid argument '$arg'. Exiting." && exit 1;;
+esac
